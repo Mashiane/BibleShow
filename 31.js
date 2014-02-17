0fbe567ffae51275ff02d4d847a964f180f25aa5
Mashiane <anele@mbangas.com>
2014-02-17 13:15:35 +0200

    Beta 9
    Removed background pic

--- 31.js
+++ 31.js
@@ -1,5 +1,4 @@
 
 frmVerses.innerHTML=[  NSB.HeaderBar_jqm14('versesT', 'Verses', '', 'home', 'left', '', 'check', 'right', ' style="" class=" "'),
   "<textarea id='txtVerses' class='ui-mini  ' name='' style='display:block; margin-top:0px; text-align:left; font-size:14px;; font-family:helvetica; font-style:; font-weight:; color:; background-color:white;-webkit-overflow-scrolling:touch; max-height:185px; '  autocorrect='off'  autocomplete='off'  autocapitalize='off' data-autogrow=false readonly=\'readonly\' placeholder=''></textarea>",
-  "<label id='lblVerse' class='' style='text-align:left;  font-size:16px; font-family:helvetica; font-style:normal; font-weight:bold; color:white; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >Label</label>",
   ].join('');
