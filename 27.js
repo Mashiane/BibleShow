097220075987da2f52ed9d42dac1b77b60395285
Mashiane <anele@mbangas.com>
2014-02-17 13:00:17 +0200

    Beta 8
    Fixed bug with Bible Promises and put a search on them

--- 27.js
+++ 27.js
@@ -1,4 +1,4 @@
 
-frmPromises.innerHTML=[  NSB.List_jqm14('promisesL', 'ul', '', '', 'b', '', '', 'N,N,N', 'style=', 'class=" "', '100%', true, false, 'false', 'arrow-r', 'false', '', 'false', 'false'),
+frmPromises.innerHTML=[  NSB.List_jqm14('promisesL', 'ul', '', '', 'b', '', '', 'N,N,N', 'style=', 'class=" "', '100%', true, false, 'false', 'arrow-r', 'true', 'Search...', 'false', 'false'),
   NSB.HeaderBar_jqm14('promisesT', 'Bible Promises', '', 'home', 'left', '', 'refresh', 'right', ' style="" class=" "'),
   ].join('');
--- 29.js
+++ 29.js
@@ -1,4 +1,4 @@
 
 frmPromise.innerHTML=[  NSB.List_jqm14('promiseL', 'ul', '', '', 'b', '', '', 'N,N,N', 'style=', 'class=" "', '100%', true, false, 'false', 'arrow-r', 'false', '', 'false', 'false'),
-  NSB.HeaderBar_jqm14('promiseT', 'Bible Promise', '', 'home', 'left', '', 'false', 'right', ' style="" class=" "'),
+  NSB.HeaderBar_jqm14('promiseT', 'Bible Promise', '', 'home', 'left', '', 'refresh', 'right', ' style="" class=" "'),
   ].join('');
