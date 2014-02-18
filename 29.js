0cee8be11b50182e7279de6fadd50061d703f772
Mashiane <anele@mbangas.com>
2014-02-18 23:37:34 +0200

    Beta 15

--- 29.js
+++ 29.js
@@ -1,5 +1,4 @@
 
-frmPromises.innerHTML=[  NSB.List_jqm14('promisesL', 'ul', '', '', 'b', '', '', 'N,N,N', 'style=', 'class=" "', '60%', true, false, 'false', 'arrow-r', 'false', 'Search...', 'false', 'false'),
+frmPromises.innerHTML=[  NSB.List_jqm14('promisesL', 'ul', '', '', 'b', '', '', 'N,N,N', 'style=', 'class=" "', '100%', true, false, 'false', 'arrow-r', 'false', 'Search...', 'false', 'false'),
   NSB.HeaderBar_jqm14('promisesT', 'Bible Promises', '', 'home', 'left', '', 'refresh', 'right', ' style="" class=" "'),
-  NSB.List_jqm14('promiseL', 'ul', '', '', 'b', '', '', 'N,N,N', 'style=', 'class=" "', '40%', true, false, 'false', 'arrow-r', 'false', '', 'false', 'false'),
   ].join('');
